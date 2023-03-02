8a635f0fadd0e1f82735d5bfd4dc5dc67347d4e5
Taehee <mix264@naver.com>
2023-03-02 23:57:59 +0900

    Merge branch 'master' of https://github.com/TaeheeMin/oracle-jdbc.git

--- src/main/java/util/DBUtil.java
+++ src/main/java/util/DBUtil.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBUtil {
 	public static Connection getConnection() throws Exception {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","gdj58","java1234");
+		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@193.122.124.61:8080:xe","admin","java1234Gdj58");
 		//System.out.println("연결 성공");
 		conn.setAutoCommit(false);
 		return conn;
